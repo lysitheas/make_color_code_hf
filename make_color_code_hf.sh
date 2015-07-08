e6e6962eefdc572eadf0dc7fab371c5b6786a892
lysitheas <tadminc.weadming@laposte.net>
2015-07-08 16:29:59 +0200

    version debugée

--- make_color_code_hf.sh
+++ make_color_code_hf.sh
@@ -30,7 +30,7 @@ if test -z "$2"; then
 echo "usage      
 make_color_code_hf.sh SOURCE CIBLE"
 echo "
-encader votre code avec des balise tel que [<couleur>]...[/<couleur>]"
+encader votre code avec des balise tel que \[<couleur>\]...\[/<couleur>\]"
 echo "Couleurs possible :"
 echo "
 rouge
@@ -46,27 +46,28 @@ else
 	cp $1 $name_file
 	
 
+	
 
 	#sed pour les commentaires en gris 
 
-	sed -i -e "s/\(\#\#*.*\)/[#BDBDBD][i]\1[\/i][\/#BDBDBD]/g" $1
-	
+	sed -i -e "s/\(\#\#*.*\)/\[#8D6E63\]\[i\]\1\[\/i\]\[\/#8D6E63\]/g" $1
+
 	#sed brut de remplacement des couleurs
-	sed -i -e "s/vert/#8BC34A/g" $1
-	sed -i -e "s/rouge/#E53935/g" $1
-	sed -i -e "s/bleu/#1565C0/g" $1
-	sed -i -e "s/jaune/#FFC107/g" $1
-	sed -i -e "s/violet/#9C27B0/g" $1
+	sed -i -e "s/\[vert\]\(.*\)\[\/vert\]/\[#8BC34A\]\1\[\/#8BC34A\]/g" $1
+	sed -i -e "s/\[rouge\]\(.*\)\[\/rouge\]/\[#E53935\]\1\[\/#E53935\]/g" $1
+	sed -i -e "s/\[bleu\]\(.*\)\[\/bleu\]/\[#1565C0\]\1\[\/#1565C0\]/g" $1
+	sed -i -e "s/\[jaune\]\(.*\)\[\/jaune\]/\[#FFC107\]\1\[\/#FFC107\]/g" $1
+	sed -i -e "s/\[violet\]\(.*\)\[\/violet\]/\[#9C27B0\]\1\[\/#9C27B0\]/g" $1
 
 
-	#sed de remplacement des chiffres en violet
 
-	sed -i -e "s/\([0-9][0-9]*\)/[#9C27B0]\1[\/#9C27B0]/g" $1
+
+
 
 	#sed de remplacement du texte entre guillemet quote et paranthese en vert
-	sed -i -e "s/\(\"[a-zA-Z][a-zA-Z]*\"\)/[#8BC34A][b]\1[\/b][\/#8BC34A]/g" $1
+	#sed -i -e "s/\(\"\[a-zA-Z\]\[a-zA-Z\]*\"\)/\[#8BC34A\]\[b\]\1\[\/b\]\[\/#8BC34A\]/g" $1
 
-	sed -i -e "s/\(([a-zA-Z][a-zA-Z]*)\)/[#009688][b]\1[\/b][\/#009688]/g" $1
+	#sed -i -e "s/\((\[a-zA-Z0-9\]\[a-zA-Z0-9\]*)\)/\[#9C27B0\]\[b\]\1\[\/b\]\[\/#9C27B0\]/g" $1
 	
 	
 
