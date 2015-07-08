fef92bbac82bc2b250adb0771abd9f48d46104be
lysitheas <tadminc.weadming@laposte.net>
2015-07-08 15:57:32 +0200

    Support les commentaire

--- make_color_code_hf.sh
+++ make_color_code_hf.sh
@@ -45,6 +45,12 @@ else
 	name_file=$1$add
 	cp $1 $name_file
 	
+
+
+	#sed pour les commentaires en gris 
+
+	sed -i -e "s/\(\#\#*.*\)/[#BDBDBD][i]\1[\/i][\/#BDBDBD]/g" $1
+	
 	#sed brut de remplacement des couleurs
 	sed -i -e "s/vert/#8BC34A/g" $1
 	sed -i -e "s/rouge/#E53935/g" $1
@@ -61,6 +67,7 @@ else
 	sed -i -e "s/\(\"[a-zA-Z][a-zA-Z]*\"\)/[#8BC34A][b]\1[\/b][\/#8BC34A]/g" $1
 
 	sed -i -e "s/\(([a-zA-Z][a-zA-Z]*)\)/[#009688][b]\1[\/b][\/#009688]/g" $1
+	
 	
 
 ######################################################
